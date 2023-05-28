1a94cb3d26e76cf971164a2984ddc152328e8b6f
dams4k <dams4k@users.noreply.github.com>
2023-05-28 11:08:06 +0200

    lava's speed was increasing even when the game havn't started

--- lava.gd
+++ lava.gd
@@ -5,9 +5,17 @@ var MAX_SPEED = 1250
 var SPEED_FACTOR = 1.2
 var TIMER_FACTOR = 1.75
 
-var can_move = false
+var can_move = false:
+	set(v):
+		can_move = v
+		timer.start()
 
 @onready var timer = $Timer
+
+func _ready():
+	if can_move:
+		timer.start()
+
 
 func _process(delta):
 	if can_move:
